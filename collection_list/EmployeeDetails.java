3d977c1c81bf7540756ed06ad538e9ca277cc0c7
vanshitamanral <44901776+vanshitamanral@users.noreply.github.com>
2021-07-14 10:30:25 +0530

    Update EmployeeDetails.java

--- collection_list/EmployeeDetails.java
+++ collection_list/EmployeeDetails.java
@@ -1,3 +1,6 @@
+/*
+Create an ArrayList of Employee( id,name,address,sal) objects and search for particular Employee object based on id number
+*/
 package collection_list;
 
 import java.util.ArrayList;
