f1ea2461132ae79cca5ba97ff088e7fc9fabd649
vanshitamanral <44901776+vanshitamanral@users.noreply.github.com>
2021-07-14 10:31:12 +0530

    Update Employee_vector.java

--- collection_list/Employee_vector.java
+++ collection_list/Employee_vector.java
@@ -1,3 +1,6 @@
+/*
+Write a program that will have a Vector which is capable of storing emp objects. Use an Iterator and enumeration to list all the elements of the Vector.
+*/
 package collection_list;
 
 import java.util.ArrayList;
